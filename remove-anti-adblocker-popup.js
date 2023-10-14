6401c8d644a7ab2cceb02f78011df29a610c8922
IncroyablePix <18048162+IncroyablePix@users.noreply.github.com>
2023-10-15 00:04:50 +0200

    Fixed pause at the begining

--- remove-anti-adblocker-popup.js
+++ remove-anti-adblocker-popup.js
@@ -12,6 +12,15 @@
 //- The @grant directives are needed to restore the proper sandbox.
 
 (() => {
+    let lastPageChangeTimestamp = new Date();
+
+    function calculateTimeDelta() {
+        const currentTime = new Date();
+        const timeDelta = currentTime - lastPageChangeTimestamp;
+        const timeDeltaInSeconds = timeDelta / 1000;
+        lastPageChangeTimestamp = currentTime; // Update the variable with current time
+    }
+
 	function waitForElement(selector) {
 		return new Promise(resolve => {
 			if (document.querySelector(selector)) {
@@ -32,12 +41,49 @@
 		});
 	}
 
+    function restartAfterForcePause() {
+        let currentTime = new Date();
+        let timeDelta = currentTime - lastPageChangeTimestamp;
+        let timeDeltaInSeconds = timeDelta / 1000;
+
+        if (timeDeltaInSeconds < 2) {
+            const video = document.querySelector(".video-stream");
+            video?.play();
+        }
+    }
+
 	function removeNode(node) {
 		node.remove();
-        const video = document.querySelector(".video-stream");
-        video?.play();
+        restartAfterForcePause();
 	}
+
+	function preservePlayingState() {
+        const video = document.querySelector(".video-stream");
+		video?.addEventListener("pause", event => {
+			restartAfterForcePause();
+		});
+	}
+
+    function observePageChange() {
+        const videoElement = document.querySelector('.video-stream');
+        const src = videoElement?.getAttribute?.("src") ?? "";
+
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.type === "attributes" && mutation.attributeName === "src") {
+                    const newSrc = mutation.target.getAttribute("src");
+                    calculateTimeDelta();
+                }
+            });
+        });
+
+        const config = { attributes: true, attributeFilter: ["src"] };
+        observer.observe(videoElement, config);
+    }
 
 	waitForElement("ytd-popup-container").then(removeNode);
 	waitForElement("tp-yt-iron-overlay-backdrop").then(removeNode);
+    calculateTimeDelta();
+    observePageChange();
+	preservePlayingState();
 })();
